a3474ed15f22948e931f76596e1fb473bbb89920
sofiacottone <cottonesofia@outlook.it>
2024-06-06 15:56:59 +0200

    feat: add migration to create project_technology pivot table

---
+++ database/migrations/2024_06_06_133853_create_project_technology_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            // create first column
+            $table->unsignedBigInteger('project_id')->nullable();
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+
+            // create second column
+            $table->unsignedBigInteger('technology_id')->nullable();
+
+            $table->foreign('technology_id')
+                ->references('id')
+                ->on('technologies')
+                ->onDelete('cascade');
+
+            // add primary key to both columns
+            $table->primary(['project_id', 'technology_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
